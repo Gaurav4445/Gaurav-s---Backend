56031064215457555dea687b883015bbfbce1005
Gaurav Hukkerikar <gaurav.hukk4445@gmail.com>
2026-03-15 00:18:50 +0530

    fix linux case sensitive imports

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,12 +1,12 @@
 import asyncHandler from "../utils/asyncHandler.js";
-import ApiError from "../utils/apiError.js";
+import ApiError from "../utils/ApiError.js";
 import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
 import { Comment } from "../models/comment.model.js";
 import {
     uploadOnCloudinary,
     deleteOnCloudinary
-} from "../utils/cloudinary.js";
+} from "../utils/Cloudinary.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import mongoose, { isValidObjectId } from "mongoose";
 import { Like } from "../models/like.model.js";
